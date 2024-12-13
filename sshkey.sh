11db41283e75a11c7e7f71c0568dffba3b1e6a56
7 <134904683+leuxinovo@users.noreply.github.com>
2024-12-13 17:25:13 +0800

    Update sshkey.sh

--- sshkey.sh
+++ sshkey.sh
@@ -2,8 +2,8 @@
 
 # 欢迎信息
 echo "=============================================="
-echo " 欢迎使用 Leu SSH 公钥一键脚本"
-echo " 本脚本将从 GitHub 获取指定用户的 SSH 公钥并配置到当前 VPS"
+echo " 欢迎使用 Leu SSH公钥一键脚本"
+echo " 本脚本将从GitHub 获取指定用户的SSH公钥并配置到当前VPS"
 echo "=============================================="
 
 # 检查是否提供了 GitHub 用户名
@@ -29,18 +29,18 @@ GITHUB_KEYS_URL="https://github.com/$GITHUB_USERNAME.keys"
 SSH_KEYS=$(curl -s "$GITHUB_KEYS_URL")
 
 if [ -z "$SSH_KEYS" ]; then
-    echo "未能获取到 GitHub 用户 $GITHUB_USERNAME 的 SSH 公钥。"
-    echo "请检查用户名是否正确，或确保该用户在 GitHub 上有 SSH 公钥。"
+    echo "未能获取到GitHub用户$GITHUB_USERNAME的SSH公钥"
+    echo "请检查用户名是否正确 或确保该用户在GitHub上有SSH公钥"
     exit 2
 fi
 
 # 将 SSH 公钥写入 authorized_keys
-echo "写入 SSH 公钥到 $AUTHORIZED_KEYS"
+echo "写入SSH公钥到$AUTHORIZED_KEYS"
 echo "$SSH_KEYS" >> "$AUTHORIZED_KEYS"
 
 # 确保 authorized_keys 的权限正确
 chmod 600 "$AUTHORIZED_KEYS"
 
 echo "=============================================="
-echo "操作完成！GitHub 用户 $GITHUB_USERNAME 的 SSH 公钥已成功添加到 $AUTHORIZED_KEYS"
+echo "操作完成！GitHub用户$GITHUB_USERNAME的SSH公钥已成功添加到$AUTHORIZED_KEYS"
 echo "=============================================="
