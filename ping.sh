dd30ee382d0daa538fa2206f906c63375a222f4b
7 <134904683+leuxinovo@users.noreply.github.com>
2025-01-09 19:32:48 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -19,32 +19,3 @@ echo "中国地区延迟测试"
 echo "===================="
 echo ""
 
-# 初始化计数器
-counter=0
-
-# 遍历城市并测试延迟
-for city in "${cities[@]}"; do
-    # 提取城市名称和 IP 地址
-    city_name=$(echo $city | cut -d':' -f1)
-    ip_address=$(echo $city | cut -d':' -f2)
-    
-    # 执行 ping 命令并获取平均延迟时间
-    ping_result=$(ping -c 4 $ip_address | tail -n 1 | awk '{print $4}' | cut -d '/' -f 2)
-
-    # 如果没有成功获取延迟时间，设置为 "N/A"
-    if [ -z "$ping_result" ]; then
-        ping_result="N/A"
-    fi
-
-    # 输出结果，每三列换行
-    echo -n "$city_name: $ping_result ms  "
-    
-    # 每三个城市换一行
-    ((counter++))
-    if [ $counter -eq 3 ]; then
-        echo ""
-        counter=0
-    fi
-done
-
-echo "===================="
