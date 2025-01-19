044e58b9124f89ee40cbd6fae9506c718e8dc6c8
7 <134904683+leuxinovo@users.noreply.github.com>
2025-01-19 20:47:51 +0800

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -9,24 +9,24 @@ echo -e "${orange}-------------------------------------${reset}"
 
 # 停止 Nezha Agent 服务
 echo "正在停止 Nezha Agent 服务..."
-systemctl stop nezha-agent 2>/dev/null || echo "Nezha Agent 服务未运行。"
+systemctl stop nezha-agent 2>/dev/null || echo "nezha-agent服务未运行。"
 
 # 禁用开机自动启动
 echo "正在禁用 Nezha Agent 开机自动启动..."
-systemctl disable nezha-agent 2>/dev/null || echo "Nezha Agent 未设置为开机启动。"
+systemctl disable nezha-agent 2>/dev/null || echo "nezha-agent未设置为开机启动。"
 
 # 删除 Nezha Agent 二进制文件
 if [ -d "/opt/nezha/agent" ]; then
-    echo "正在删除 Nezha Agent 二进制文件..."
+    echo "正在删除 nezha-agent二进制文件..."
     rm -rf /opt/nezha/agent
     echo "二进制文件已删除。"
 else
-    echo "未在 /opt/nezha/agent 目录中找到 Nezha Agent 二进制文件。"
+    echo "未在 /opt/nezha/agent 目录中找到 nezha-agent二进制文件。"
 fi
 
 # 删除 Nezha Agent 服务文件
 if [ -f "/etc/systemd/system/nezha-agent.service" ]; then
-    echo "正在删除 Nezha Agent 服务文件..."
+    echo "正在删除 nezha-agent服务文件..."
     rm /etc/systemd/system/nezha-agent.service
     echo "服务文件已删除。"
 else
@@ -37,6 +37,6 @@ fi
 echo "正在重新加载 systemd 守护进程配置..."
 systemctl daemon-reload
 
-echo "Nezha Agent 已成功移除。"
+echo "nezha-agent 已成功移除。"
 echo -e "${orange}更多一键脚本GitHub：https://github.com/leuxinovo/clearvps${reset}"
 echo -e "${orange}-------------------------------------${reset}"
